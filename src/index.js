3315151f4718d4066691ad8cd38babe0d9f6572f
genie <genie88@163.com>
2018-04-04 17:47:03 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// @flow
+export * from 'toxic-utils';
+export * from 'toxic-predicate-functions';
+export * from './utils';
+export * from './events';
+export * from './dom';
+import Log from './log';
+import UAParser from 'ua-parser-js';
+export { Log, UAParser };
