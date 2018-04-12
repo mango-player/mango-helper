ffc12e9715543139987ead063c8da9f2c83577df
genie <genie88@163.com>
2018-04-12 11:08:14 +0800

    增加utils函数

--- src/utils.js
+++ src/utils.js
@@ -364,3 +364,17 @@ export function setLocalStorage(key: string, val: string) {
     }
   }
 }
+
+
+export function getUrlParam(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象  
+  var r = window.location.search.substr(1).match(reg);  //匹配目标参数  
+  if (r != null) return unescape(r[2]); return null; //返回参数值  
+}
+
+
+export function getCookie(name) {
+  var r = new RegExp("(?:^|;+|\\s+)" + name + "=([^;]*)");
+  var m = document.cookie.match(r);
+  return (!m ? "" : m[1]);
+}
